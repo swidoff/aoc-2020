473f8544674f43cfd0bd798e1f155d9ea2046e9e
Seth W <swidoff@gmail.com>
2020-12-23 22:20:12 -1000

    day 23. Inlined the CupList struct, but that didn't improve the performance. I guess the Rust compiler was already inlining the methods, since they were only used at one call site each.

--- src/day23.rs
+++ src/day23.rs
@@ -58,79 +58,50 @@ fn play_cups(input: &Vec<usize>, moves: usize) -> String {
 }
 
 /// Part 2
-
-/// Maps a cup value to the next cup value in the circular list.
-/// A value of 0 means the cup is temporarily out of the list.
-struct CupList {
-    next: Vec<usize>,
-}
-
-impl CupList {
-    fn new(input: &Vec<usize>) -> CupList {
-        let mut next = vec![0; input.len() + 1];
-
-        for (i, v) in input.iter().enumerate() {
-            let next_cup = input[(i + 1) % input.len()];
-            next[*v] = next_cup;
-        }
-
-        CupList { next }
-    }
-
-    /// Returns the next cup in the list.
-    fn get_next(&self, cup: usize) -> usize {
-        self.next[cup]
-    }
-
-    fn contains(&self, cup: usize) -> bool {
-        self.next[cup] != 0
-    }
-
-    /// Removes the next cup after `cup` and returns its value.
-    fn remove_next(&mut self, cup: usize) -> usize {
-        let next_cup = self.next[cup];
-        let new_next_cup = self.next[next_cup];
-        self.next[next_cup] = 0;
-        self.next[cup] = new_next_cup;
-        next_cup
-    }
-
-    /// Inserts a `next_cup` into the list right after `cup.`
-    fn insert_next(&mut self, cup: usize, next_cup: usize) {
-        let next_value = self.next[cup];
-        self.next[next_cup] = next_value;
-        self.next[cup] = next_cup;
-    }
-}
-
 fn play_cups_faster(input: &Vec<usize>, moves: usize) -> usize {
-    let mut node_map = CupList::new(input);
+    // For each cup, the vaue of the next cup in the circular list.
+    // The value is zero if the cup as been removed from the list.
+    let mut next = vec![0; input.len() + 1];
+
+    for (i, v) in input.iter().enumerate() {
+        let next_cup = input[(i + 1) % input.len()];
+        next[*v] = next_cup;
+    }
 
     let mut current_value = input[0];
     let mut q = VecDeque::with_capacity(3);
     for _ in 0..moves {
+        // Remove the 3 cup after the current cups and push them to the q.
         for _ in 1..4 {
-            q.push_back(node_map.remove_next(current_value));
+            let next_cup = next[current_value];
+            let new_next_cup = next[next_cup];
+            next[next_cup] = 0;
+            next[current_value] = new_next_cup;
+            q.push_back(next_cup);
         }
 
+        // Find the next smallest value after current_value in the list, wrapping around if necessary.
         let mut dest_value = current_value - 1;
-        while !node_map.contains(dest_value) {
+        while next[dest_value] == 0 {
             if dest_value == 0 {
-                dest_value = input.len() as usize;
+                dest_value = input.len();
             } else {
                 dest_value -= 1
             }
         }
 
-        while let Some(v) = q.pop_back() {
-            node_map.insert_next(dest_value, v);
+        // Insert the three cups in their original order right after the dest_value.
+        while let Some(cup) = q.pop_back() {
+            let next_cup = next[dest_value];
+            next[dest_value] = cup;
+            next[cup] = next_cup;
         }
 
-        current_value = node_map.get_next(current_value);
+        current_value = next[current_value];
     }
 
-    let v1 = node_map.get_next(1);
-    let v2 = node_map.get_next(v1);
+    let v1 = next[1];
+    let v2 = next[v1];
     v1 * v2
 }
 
